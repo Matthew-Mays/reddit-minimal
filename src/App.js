0967bc86cbd1b27c462902f9811825b9a173dd4d
Matthew-Mays <matthew.c.mays96@gmail.com>
2022-09-23 12:51:29 -0500

    break header out to react component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import React from 'react';
-import redditLogo from './images/reddit-logo.png';
+import { Header } from './Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className='logo-container'>
-          <img className='header-logo' src={redditLogo} alt='reddit logo' />
-          <h3 className='header-title'>RedditMinimal</h3>
-        </div>
-      </header>
+      <Header />
       <section className='post-container'></section>
       <section className='subreddit-container'></section>
     </div>
